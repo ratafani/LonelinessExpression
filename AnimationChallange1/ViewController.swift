4a8219a7b9d42f6c9b7e36309fa07f5f31001339
muhammadtafanirabbani <ratafani@gmail.com>
2019-05-14 20:59:36 +0700

    Initial Commit

---
+++ AnimationChallange1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationChallange1
+//
+//  Created by Muhammad Tafani Rabbani on 14/05/19.
+//  Copyright © 2019 Muhammad Tafani Rabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
